af9ffcd91a471e4e577441d8c6af27bcb800a99a
linzhiqing <15621555735@126.com>
2016-09-28 10:25:02 +0800

    Initial Commit

---
+++ MySwift/RequestTask.swift
@@ -0,0 +1,13 @@
+//
+//  RequestTask.swift
+//  MySwift
+//
+//  Created by 易点租 on 16/9/14.
+//  Copyright © 2016年 易点租. All rights reserved.
+//
+
+import Cocoa
+
+class RequestTask: NSObject {
+
+}
---
+++ MySwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MySwift
+//
+//  Created by 易点租 on 16/9/14.
+//  Copyright © 2016年 易点租. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
